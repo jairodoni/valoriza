d713b580504d4843a720ba3a80ac709916995d1e
Jairo Doni <jairo.doni97@gmail.com>
2021-06-23 16:53:29 -0300

    Middlewares e Criação de Tags

---
+++ src/models/Tag.ts
@@ -0,0 +1,33 @@
+import {
+  Column,
+  CreateDateColumn,
+  Entity,
+  PrimaryColumn,
+  UpdateDateColumn,
+} from "typeorm";
+
+import { v4 as uuid } from "uuid";
+
+@Entity("tags")
+class Tag {
+  @PrimaryColumn()
+  readonly id: string;
+
+  @Column()
+  name: string;
+
+  @CreateDateColumn()
+  created_at: Date;
+
+  @UpdateDateColumn()
+  updated_at: Date;
+
+  constructor() {
+    //toda vez q cria um novo usuario cria-se um novo id com "uuid"
+    if (!this.id) {
+      this.id = uuid();
+    }
+  }
+}
+
+export { Tag };
